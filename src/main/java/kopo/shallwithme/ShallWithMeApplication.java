f7d12ef0c1dc4c92c5576c500ca25a65377f7666
hjhjhj0917 <abc22842199@gmail.com>
2025-07-24 13:16:24 +0900

    commit

---
+++ src/main/java/kopo/shallwithme/ShallWithMeApplication.java
@@ -0,0 +1,13 @@
+package kopo.shallwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShallWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShallWithMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kopo/shallwithme/ShallWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package kopo.shallwithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShallWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
