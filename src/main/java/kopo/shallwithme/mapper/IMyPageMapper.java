4c2f7d138fe592b456d1faceda97f81ed3bd38e8
hjhjhj0917 <abc22842199@gmail.com>
2025-09-09 16:26:49 +0900

    회원탈퇴 기능 작업중

--- src/main/java/kopo/shallwithme/mapper/IMyPageMapper.java
+++ src/main/java/kopo/shallwithme/mapper/IMyPageMapper.java
@@ -6,5 +6,9 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface IMyPageMapper {
 
+    // 마이페이지 접속을 위한 비밀번호 확인
     UserInfoDTO pwCheck(UserInfoDTO pDTO) throws Exception;
+
+    // 회원 탈퇴를 진행하기위한 이메일 확인
+    UserInfoDTO emailCheck(UserInfoDTO pDTO) throws Exception;
 }
