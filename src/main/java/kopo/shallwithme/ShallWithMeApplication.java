2aa0c6d24690f5d670eea9563ac454ea2ceaa854
hjhjhj0917 <abc22842199@gmail.com>
2025-10-25 18:27:07 +0900

    추가

--- src/main/java/kopo/shallwithme/ShallWithMeApplication.java
+++ src/main/java/kopo/shallwithme/ShallWithMeApplication.java
@@ -3,9 +3,11 @@ package kopo.shallwithme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRedisHttpSession
 public class ShallWithMeApplication {
 
     public static void main(String[] args) {
